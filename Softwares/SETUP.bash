200678f5f4ab66319569cc01b2e7d04d0504cc1b
1054 <liudz1054@gmail.com>
2017-11-09 15:54:00 +0100

    Updates

--- Softwares/SETUP.bash
+++ Softwares/SETUP.bash
@@ -14,13 +14,31 @@ fi
 if [[ $PATH != *"$ALMACOSMOS/3rd/bin"* ]]; then
     export PATH="$ALMACOSMOS/3rd/bin:$PATH"
 fi
-if [[ x"$PYTHONPATH" != x*"$ALMACOSMOS/3rd/lib/python2.7/site-packages"* ]]; then
-    if [[ -z "$PYTHONPATH" ]]; then
-        export PYTHONPATH="$ALMACOSMOS/3rd/lib/python2.7/site-packages:$ALMACOSMOS/3rd/lib/python2.7/site-packages/bdsf-1.8.12-py2.7-macosx-10.12-x86_64.egg"
-    else
-        export PYTHONPATH="$ALMACOSMOS/3rd/lib/python2.7/site-packages:$ALMACOSMOS/3rd/lib/python2.7/site-packages/bdsf-1.8.12-py2.7-macosx-10.12-x86_64.egg:$PYTHONPATH"
+if [[ $(uname) == "Darwin" ]]; then
+    if [[ $PATH != *"$ALMACOSMOS/3rd_mac/bin"* ]]; then
+        export PATH="$ALMACOSMOS/3rd_mac/bin:$PATH"
+    fi
+    if [[ x"$PYTHONPATH" != x*"$ALMACOSMOS/3rd_mac/lib/python2.7/site-packages"* ]]; then
+        if [[ -z "$PYTHONPATH" ]]; then
+            export PYTHONPATH="$ALMACOSMOS/3rd_mac/lib/python2.7/site-packages:$ALMACOSMOS/3rd_mac/lib/python2.7/site-packages/bdsf-1.8.12-py2.7-macosx-10.12-x86_64.egg"
+        else
+            export PYTHONPATH="$ALMACOSMOS/3rd_mac/lib/python2.7/site-packages:$ALMACOSMOS/3rd_mac/lib/python2.7/site-packages/bdsf-1.8.12-py2.7-macosx-10.12-x86_64.egg:$PYTHONPATH"
+        fi
+    fi
+else
+    if [[ $PATH != *"$ALMACOSMOS/3rd_linux/bin"* ]]; then
+        export PATH="$ALMACOSMOS/3rd_linux/bin:$PATH"
+    fi
+    if [[ x"$PYTHONPATH" != x*"$ALMACOSMOS/3rd_linux/lib/python2.7/site-packages"* ]]; then
+        if [[ -z "$PYTHONPATH" ]]; then
+            export PYTHONPATH="$ALMACOSMOS/3rd_linux/lib/python2.7/site-packages:$ALMACOSMOS/3rd_linux/lib64/python2.7/site-packages:$ALMACOSMOS/3rd_linux/lib64/python2.7/site-packages/bdsf-1.8.12-py2.7-linux-x86_64.egg"
+        else
+            export PYTHONPATH="$ALMACOSMOS/3rd_linux/lib/python2.7/site-packages:$ALMACOSMOS/3rd_linux/lib64/python2.7/site-packages:$ALMACOSMOS/3rd_linux/lib64/python2.7/site-packages/bdsf-1.8.12-py2.7-linux-x86_64.egg:$PYTHONPATH"
+        fi
     fi
 fi
+    
+
 #
 # LIST
 ALMACOSMOSCMD=("almacosmos-sky-coverage" "almacosmos-fits-image-to-coverage-polyogn" "almacosmos-analyze-fits-image-pixel-histogram" "almacosmos-generate-PSF-Gaussian-2D" "almacosmos-highz-galaxy-crossmatcher" "almacosmos-highz-galaxy-crossmatcher-read-results")
