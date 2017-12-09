49963be29b85908a1a7bdf5fcf062bda16532722
1054 <liudz1054@gmail.com>
2017-12-09 02:51:58 +0100

    Updates

--- Pipeline/a3cosmos-MC-simulation-physically-motivated/do_simulation/a_dzliu_code_for_rsync_with_ISAAC.sh
+++ Pipeline/a3cosmos-MC-simulation-physically-motivated/do_simulation/a_dzliu_code_for_rsync_with_ISAAC.sh
@@ -22,4 +22,11 @@ rsync -avz -r --stats --progress -e "ssh -A -t gate.rzg.mpg.de ssh" \
         "isaac1.bc.rzg.mpg.de:/u/$USER/Work/AlmaCosmos/Photometry/ALMA_full_archive/Simulation_by_Daizhong_2/Simulated/" \
         "$HOME/Work/AlmaCosmos/Simulations/Physical_MC_sim/20171105/Simulated"
 
+mkdir -p "$HOME/Work/AlmaCosmos/Simulations/Physical_MC_sim/20171105/Recovered"
+
+rsync -avz -r --stats --progress -e "ssh -A -t gate.rzg.mpg.de ssh" \
+        --include '**/*' \
+        "isaac1.bc.rzg.mpg.de:/u/$USER/Work/AlmaCosmos/Photometry/ALMA_full_archive/Simulation_by_Daizhong_2/Recovered/" \
+        "$HOME/Work/AlmaCosmos/Simulations/Physical_MC_sim/20171105/Recovered"
+
 
