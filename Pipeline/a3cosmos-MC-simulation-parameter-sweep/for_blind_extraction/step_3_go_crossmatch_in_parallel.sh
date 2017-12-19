3b3bf0fa368af401fa4008bc646111d620166288
1054 <liudz1054@gmail.com>
2017-12-19 15:20:23 +0100

    Updates

---
+++ Pipeline/a3cosmos-MC-simulation-parameter-sweep/for_blind_extraction/step_3_go_crossmatch_in_parallel.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# 
+
+source $(dirname $(dirname $(dirname $(dirname "${BASH_SOURCE[0]}"))))/Softwares/SETUP.bash
+
+
+if [[ $(hostname) != "aida42198" ]]; then
+    exit
+fi
+
+
+
+cd /disk1/ALMA_COSMOS/A3COSMOS/simulations/
+
+mkdir statistics_PyBDSM
+
+if [[ -f "statistics_PyBDSM/list_of_commands.txt" ]]; then rm "statistics_PyBDSM/list_of_commands.txt"; fi
+
+for (( i=1; i<10; i++ )); do
+    
+    echo $(dirname "${BASH_SOURCE[0]}")/step_3_go_crossmatch.py "$i" >> "statistics_PyBDSM/list_of_commands.txt"
+    
+done
+
+#almacosmos_cmd_run_in_parallel "statistics_PyBDSM/list_of_commands.txt"
+
+
+
+
+
