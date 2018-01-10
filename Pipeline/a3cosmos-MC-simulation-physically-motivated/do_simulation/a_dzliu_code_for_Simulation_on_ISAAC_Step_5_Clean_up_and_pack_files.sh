9dd92c70aa0a35577835f47cf90fcbbae2f226b4
1054 <liudz1054@gmail.com>
2018-01-10 08:00:19 +0100

    Updates

---
+++ Pipeline/a3cosmos-MC-simulation-physically-motivated/do_simulation/a_dzliu_code_for_Simulation_on_ISAAC_Step_5_Clean_up_and_pack_files.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# 
+
+
+# 
+# Read Recovered catalog
+# 
+IFS=$'\n' read -d '' -r -a FitsNames < "list_projects.txt"
+
+for (( i=0; i<${#FitsNames[@]}; i++ )); do
+    
+    FitsName="${FitsNames[i]}"
+    echo "${FitsNames[i]} ($((i+1))/${#FitsNames[@]})"
+    
+    # check non-COSMOS fields
+    if [[ "$FitsName" == *"2011.0.00539.S_SB1_GB1_MB1_ECDFS02_field3_sci.spw0_1_2_3"* ]] || \
+        [[ "$FitsName" == *"2011.0.00539.S_SB1_GB1_MB2_ELS01_field2_sci.spw0_1_2_3"* ]] ; then
+        echo "Warning! \"$FitsName\" is a non-COSMOS field! Skip and continue!"
+        continue
+    fi
+    
+    # check simulation directory and datatable
+    if [[ ! -f "Simulated/$FitsName/datatable_Simulated.txt" ]]; then
+        echo "Error! \"Simulated/$FitsName/datatable_Simulated.txt\" was not found!"
+        exit
+    fi
+    
+    # check recovered directory
+    if [[ ! -d "Recovered/$FitsName/" ]]; then
+        echo "Error! \"Recovered/$FitsName/\" was not found!"
+        exit
+    fi
+    
+    # cd 
+    cd "Recovered"
+    
+    # tar
+    tar -czf "$FitsName.tar.gz" "$FitsName"
+    
+    # cd back
+    cd "../"
+    
+    # cd 
+    cd "Simulated"
+    
+    # tar
+    tar -czf "$FitsName.tar.gz" "$FitsName"
+    
+    # cd back
+    cd "../"
+    
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
