97811f7ebfcca079ae613197224decd9265614fe
1054 <liudz1054@gmail.com>
2018-02-06 18:18:59 +0100

    Updates

--- Pipeline/a3cosmos-MC-simulation-physically-motivated/do_simulation/a_dzliu_code_for_Simulation_on_ISAAC_Step_5_Clean_up_and_pack_files.sh
+++ Pipeline/a3cosmos-MC-simulation-physically-motivated/do_simulation/a_dzliu_code_for_Simulation_on_ISAAC_Step_5_Clean_up_and_pack_files.sh
@@ -13,9 +13,29 @@ for (( i=0; i<${#FitsNames[@]}; i++ )); do
     echo "${FitsNames[i]} ($((i+1))/${#FitsNames[@]})"
     
     # check non-COSMOS fields
-    if [[ "$FitsName" == *"2011.0.00539.S_SB1_GB1_MB1_ECDFS02_field3_sci.spw0_1_2_3"* ]] || \
-        [[ "$FitsName" == *"2011.0.00539.S_SB1_GB1_MB2_ELS01_field2_sci.spw0_1_2_3"* ]] ; then
+    if [[ "$FitsName" == "2011.0.00539.S_"*"_ECDFS02_"* ]] || \
+        [[ "$FitsName" == "2011.0.00539.S_"*"_ELS01_"* ]] || \
+        [[ "$FitsName" == "2011.0.00539.S_"*"_ADFS01_"* ]] || \
+        [[ "$FitsName" == "2011.0.00539.S_"*"_XMM01_"* ]] || \
+        [[ "$FitsName" == "2011.0.00742.S_"*"__RX_J094144.51+385434.8__"* ]] || \
+        [[ "$FitsName" == "2012.1.00596.S_"*"_PKS0215+015_"* ]] ; then
         echo "Warning! \"$FitsName\" is a non-COSMOS field! Skip and continue!"
+        continue
+    fi
+    
+    # check very high-res. images
+    if [[ "$FitsName" == *"2015.1.00607.S_SB1_GB1_MB1_AzTEC-3_sci.spw0_1_2_3"* ]] || \
+        [[ "$FitsName" == *"2015.1.00695.S_SB1_GB1_MB1_COSMOS_824759_sci.spw0_1_2_3"* ]] || \
+        [[ "$FitsName" == *"2015.1.00695.S_SB2_GB1_MB1_COSMOS_823380_sci.spw0_1_2_3"* ]] || \
+        [[ "$FitsName" == *"2015.1.00695.S_SB3_GB1_MB1_COSMOS_822872_sci.spw0_1_2_3"* ]] || \
+        [[ "$FitsName" == *"2015.1.00695.S_SB3_GB1_MB1_COSMOS_822965_sci.spw0_1_2_3"* ]] || \
+        [[ "$FitsName" == *"2015.1.00695.S_SB4_GB1_MB1_COSMOS_810344_sci.spw0_1_2_3"* ]] || \
+        [[ "$FitsName" == *"2015.1.00695.S_SB4_GB1_MB1_COSMOS_839268_sci.spw0_1_2_3"* ]] || \
+        [[ "$FitsName" == *"2015.1.00928.S_SB3_GB1_MB1_LBG-1_sci.spw0_1_2_3"* ]] || \
+        [[ "$FitsName" == *"2015.1.01345.S_SB1_GB1_MB1_AzTEC1_sci.spw0_1_2_3"* ]] || \
+        [[ "$FitsName" == *"2015.1.01345.S_SB1_GB1_MB1_AzTEC4_sci.spw0_1_2_3"* ]] || \
+        [[ "$FitsName" == *"2015.1.01345.S_SB2_GB1_MB1_AzTEC8_sci.spw0_1_2_3"* ]] ; then
+        echo "Warning! \"$FitsName\" is a very high-res. image! Skip and continue!"
         continue
     fi
     
