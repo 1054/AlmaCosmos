f11b6694548c760c08dd61d69e25c3fdbff80f6b
1054 <liudz1054@gmail.com>
2018-02-17 11:26:59 +0100

    Updates

---
+++ Pipeline/a3cosmos-MC-simulation-parameter-sampled/step_4_do_prior_extraction_by_daizhong/task_1_make_links.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 
+
+
+if [[ $(hostname) != "aida"* ]]; then
+    echo "Sorry, this code can only be ran on aida machine!"
+    exit
+fi
+
+echo "/disk1/ALMA_COSMOS/A3COSMOS/simulations/output_GALFIT"
+
+
+
+
+
