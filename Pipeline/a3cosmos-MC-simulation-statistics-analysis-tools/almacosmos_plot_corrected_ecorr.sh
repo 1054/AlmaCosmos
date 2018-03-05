63327129d95a657756ce62f3c07e192157a39451
1054 <liudz1054@gmail.com>
2018-03-05 12:00:40 +0100

    Updates

--- Pipeline/a3cosmos-MC-simulation-statistics-analysis-tools/almacosmos_plot_corrected_ecorr.sh
+++ Pipeline/a3cosmos-MC-simulation-statistics-analysis-tools/almacosmos_plot_corrected_ecorr.sh
@@ -21,7 +21,7 @@ topcat -stilts plot2plane \
                 x1='x1' \
                 y1='ecorr' \
                 \
-                aux='x2' auxvisible=true auxlabel="Maj_{source}/Maj_{beam}" \
+                aux='x2' auxvisible=true auxmap=rdbu auxflip=true auxlabel="Maj_{source}/Maj_{beam}" \
                 \
                 layer3=function \
                 fexpr3='(x)' \
@@ -39,6 +39,18 @@ topcat -stilts plot2plane \
                 # http://www.star.bristol.ac.uk/~mbt/stilts/sun256/plot2plane-examples.html
 
 echo "Output to \"Plot_corrected_ecorr.pdf\"!"
+convert -density 150 "Plot_corrected_ecorr.pdf" "Plot_corrected_ecorr.png"
+
+
+if [[ ! -f 'datatable_applied_correction_ecorr_with_S_peak.txt' ]]; then
+topcat -stilts tmatchn \
+                nin=2 \
+                in1='datatable_applied_correction_ecorr.txt' ifmt1=ascii \
+                in2='../simu_data_input.txt' ifmt2=ascii icmd2="keepcols \"S_peak noise Maj_out Min_out Maj_beam Min_beam\"" \
+                values1='index' values2='index' \
+                matcher=exact multimode=pairs iref=1 \
+                out='datatable_applied_correction_ecorr_with_S_peak.txt'
+fi
 
 
 topcat -stilts plot2plane \
@@ -59,7 +71,7 @@ topcat -stilts plot2plane \
                 x1='S_out/e_S_out_uncorr' \
                 y1='S_out/e_S_out_corr' \
                 \
-                aux='x2' auxvisible=true auxlabel="Maj_{source}/Maj_{beam}" \
+                aux='x2' auxvisible=true auxmap=rdbu auxflip=true auxlabel="Maj_{source}/Maj_{beam}" \
                 \
                 layer3=function \
                 fexpr3='(x)' \
@@ -77,6 +89,121 @@ topcat -stilts plot2plane \
                 # http://www.star.bristol.ac.uk/~mbt/stilts/sun256/plot2plane-examples.html
 
 echo "Output to \"Plot_corrected_ecorr_SNR.pdf\"!"
+convert -density 150 "Plot_corrected_ecorr_SNR.pdf" "Plot_corrected_ecorr_SNR.png"
+
+
+topcat -stilts plot2plane \
+                xpix=500 ypix=400 \
+                insets="${margin[3]},${margin[0]},${margin[1]},${margin[2]}" \
+                xlabel="\Large \sigma_{S_{out,\,uncorr.}}" \
+                ylabel="\Large \sigma_{S_{out,\,corr.}}" \
+                xlog=true \
+                ylog=true \
+                \
+                layer1=mark \
+                shape1=open_circle \
+                shading1=aux \
+                in1='datatable_applied_correction_ecorr.txt' \
+                ifmt1=ascii \
+                icmd1='sort x2' \
+                x1='e_S_out_uncorr' \
+                y1='e_S_out_corr' \
+                \
+                aux='x2' auxvisible=true auxmap=rdbu auxflip=true auxlabel="Maj_{source}/Maj_{beam}" \
+                \
+                layer3=function \
+                fexpr3='(x)' \
+                color3=black \
+                antialias3=true \
+                thick3=1 \
+                \
+                legend=false \
+                seq="3,1" \
+                fontsize=16 \
+                texttype=latex \
+                omode=out \
+                out='Plot_corrected_ecorr_vs_uncorrected_ecorr.pdf'
+                # http://www.star.bristol.ac.uk/~mbt/stilts/sun256/plot2plane-usage.html
+                # http://www.star.bristol.ac.uk/~mbt/stilts/sun256/plot2plane-examples.html
+
+echo "Output to \"Plot_corrected_ecorr_vs_uncorrected_ecorr.pdf\"!"
+convert -density 150 "Plot_corrected_ecorr_vs_uncorrected_ecorr.pdf" "Plot_corrected_ecorr_vs_uncorrected_ecorr.png"
+
+
+topcat -stilts plot2plane \
+                xpix=500 ypix=400 \
+                insets="${margin[3]},${margin[0]},${margin[1]},${margin[2]}" \
+                xlabel="\Large \sigma_{S_{out,\,uncorr.}}" \
+                ylabel="\Large \sigma_{S_{out,\,corr.}}" \
+                xlog=true \
+                ylog=true \
+                \
+                layer1=mark \
+                shape1=open_circle \
+                shading1=aux \
+                in1='datatable_applied_correction_ecorr_with_S_peak.txt' \
+                ifmt1=ascii \
+                icmd1='sort (Min_out/Maj_beam)' \
+                x1='e_S_out_uncorr' \
+                y1='e_S_out_corr' \
+                \
+                aux='(Min_out/Maj_beam)' auxvisible=true auxmap=rdbu auxflip=true auxlabel="Min_{source}/Maj_{beam}" \
+                \
+                layer3=function \
+                fexpr3='(x)' \
+                color3=black \
+                antialias3=true \
+                thick3=1 \
+                \
+                legend=false \
+                seq="3,1" \
+                fontsize=16 \
+                texttype=latex \
+                omode=out \
+                out='Plot_corrected_ecorr_vs_uncorrected_ecorr_colored_by_Min_out.pdf'
+                # http://www.star.bristol.ac.uk/~mbt/stilts/sun256/plot2plane-usage.html
+                # http://www.star.bristol.ac.uk/~mbt/stilts/sun256/plot2plane-examples.html
+
+echo "Output to \"Plot_corrected_ecorr_vs_uncorrected_ecorr_colored_by_Min_out.pdf\"!"
+convert -density 150 "Plot_corrected_ecorr_vs_uncorrected_ecorr_colored_by_Min_out.pdf" "Plot_corrected_ecorr_vs_uncorrected_ecorr_colored_by_Min_out.png"
+
+
+topcat -stilts plot2plane \
+                xpix=500 ypix=400 \
+                insets="${margin[3]},${margin[0]},${margin[1]},${margin[2]}" \
+                xlabel="\Large \sigma_{S_{out,\,uncorr.}}" \
+                ylabel="\Large \sigma_{S_{out,\,corr.}}" \
+                xlog=true \
+                ylog=true \
+                \
+                layer1=mark \
+                shape1=open_circle \
+                shading1=aux \
+                in1='datatable_applied_correction_ecorr_with_S_peak.txt' \
+                ifmt1=ascii \
+                icmd1='sort (S_peak/noise)' \
+                x1='e_S_out_uncorr' \
+                y1='e_S_out_corr' \
+                \
+                aux='(S_peak/noise)' auxvisible=true auxmap=rdbu auxflip=true auxfunc=log auxlabel="S_{peak}/{noise}" \
+                \
+                layer3=function \
+                fexpr3='(x)' \
+                color3=black \
+                antialias3=true \
+                thick3=1 \
+                \
+                legend=false \
+                seq="3,1" \
+                fontsize=16 \
+                texttype=latex \
+                omode=out \
+                out='Plot_corrected_ecorr_vs_uncorrected_ecorr_colored_by_S_peak.pdf'
+                # http://www.star.bristol.ac.uk/~mbt/stilts/sun256/plot2plane-usage.html
+                # http://www.star.bristol.ac.uk/~mbt/stilts/sun256/plot2plane-examples.html
+
+echo "Output to \"Plot_corrected_ecorr_vs_uncorrected_ecorr_colored_by_S_peak.pdf\"!"
+convert -density 150 "Plot_corrected_ecorr_vs_uncorrected_ecorr_colored_by_S_peak.pdf" "Plot_corrected_ecorr_vs_uncorrected_ecorr_colored_by_S_peak.png"
 
 
 topcat -stilts plot2plane \
@@ -97,7 +224,7 @@ topcat -stilts plot2plane \
                 x1='S_out/e_S_out_uncorr' \
                 y1='(S_out/e_S_out_corr)/(S_out/e_S_out_uncorr)' \
                 \
-                aux='x2' auxvisible=true auxlabel="Maj_{source}/Maj_{beam}" \
+                aux='x2' auxvisible=true auxmap=rdbu auxflip=true auxlabel="Maj_{source}/Maj_{beam}" \
                 \
                 layer3=function \
                 fexpr3='(1)' \
@@ -115,14 +242,12 @@ topcat -stilts plot2plane \
                 # http://www.star.bristol.ac.uk/~mbt/stilts/sun256/plot2plane-examples.html
 
 echo "Output to \"Plot_corrected_ecorr_SNR_ratio.pdf\"!"
-
-
-
-convert -density 150 "Plot_corrected_ecorr.pdf" "Plot_corrected_ecorr.png"
-
-convert -density 150 "Plot_corrected_ecorr_SNR.pdf" "Plot_corrected_ecorr_SNR.png"
-
 convert -density 150 "Plot_corrected_ecorr_SNR_ratio.pdf" "Plot_corrected_ecorr_SNR_ratio.png"
+
+
+
+
+
 
 
 exit
