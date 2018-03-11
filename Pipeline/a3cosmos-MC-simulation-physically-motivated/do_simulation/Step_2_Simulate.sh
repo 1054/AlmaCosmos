24c2ea37729cc29970b4de7de7f364394abb1e85
1054 <liudz1054@gmail.com>
2018-03-11 14:21:30 +0100

    Updates

--- Pipeline/a3cosmos-MC-simulation-physically-motivated/do_simulation/Step_2_Simulate.sh
+++ Pipeline/a3cosmos-MC-simulation-physically-motivated/do_simulation/Step_2_Simulate.sh
@@ -183,12 +183,12 @@ for (( i=0; i<${#FitsNames[@]}; i++ )); do
         "Data/ALMA_full_archive/Calibrated_Images_by_Benjamin/$Data_Version/fits_cont_I_image_pixel_histograms/$FitsName.cont.I.image.fits.pixel.statistics.txt" \
         "Data/ALMA_full_archive/Calibrated_Images_by_Benjamin/$Data_Version/fits_cont_I_clean-beam/$FitsName.cont.I.clean-beam.fits"
         do
-        if [[ ! -f "Input_images/$(basename $file_to_download)" ]]; then
+        if [[ ! -f "Input_images/$(basename $file_to_download)" ]] && [[ ! -L "Input_images/$(basename $file_to_download)" ]]; then
             cd "Input_images/"
             almacosmos_gdownload.py "$file_to_download" #<TODO># now we do not download from Google, but directly rsync to there.
             cd "../"
         fi
-        if [[ ! -f "Input_images/$(basename $file_to_download)" ]]; then
+        if [[ ! -f "Input_images/$(basename $file_to_download)" ]] && [[ ! -L "Input_images/$(basename $file_to_download)" ]]; then
             echo "Error! Failed to get the image file \"$file_to_download\" from Google Drive! Please re-try!"
             exit 1
         fi
@@ -207,6 +207,13 @@ for (( i=0; i<${#FitsNames[@]}; i++ )); do
     obswave=$(awk "BEGIN {print 2.99792458e5/($obsfreq/1e9);}")
     if [[ "$obswave"x == ""x ]]; then
         echo "Error! Failed to get observation frequency/wavelength from the fits header keyword CRVAL3 of input fits file \"Input_images/$FitsName.cont.I.image.fits\"!"
+        exit 1
+    fi
+    
+    # get rms noise
+    rmsnoise=$(cat "Input_images/$FitsName.cont.I.image.fits.pixel.statistics.txt" | grep "Gaussian_sigma" | perl -p -e 's/.+ = ([^#]+)/\1/g')
+    if [[ "$rmsnoise"x == ""x ]]; then
+        echo "Error! Failed to get rms noise from \"Input_images/$FitsName.cont.I.image.fits.pixel.statistics.txt\"!"
         exit 1
     fi
     
@@ -235,19 +242,21 @@ for (( i=0; i<${#FitsNames[@]}; i++ )); do
                             -res "Input_images/$FitsName.cont.I.residual.fits" \
                             -gal "$Input_Galaxy_Modeling_Dir" \
                             -w "$i_w" -z "${i_z}" -lgMstar "${i_lgMstar}" -Type-SED "${i_Type_SED}" \
+                            -rms-noise $rmsnoise -snr-peak-threshold 3.0 \
                             -out "Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}"
                         
-                        if [[ ! -d "Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}" ]]; then
+                        if [[ ! -d "Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}/$FitsName" ]]; then
                             echo "Error! Failed to run \"almacosmos_simulate_image_based_on_galaxy_modeling\" and create \"Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}\" directory!"
                             exit 1
                         fi
                         
-                        cd "Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}/"
-                        cp */galaxy_model_*.txt ./
-                        cp */image_sim.fits ./
-                        cd "../../../"
+                        cd "Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}/$FitsName/"
+                        cp galaxy_model_*.txt ../
+                        if [[ -f "image_sim.fits" ]]; then cp "image_sim.fits" ../; elif [[ -f "too_faint" ]]; then cp "too_faint" ../; fi
+                        cd "../../../../"
                         
-                        if [[ ! -f "Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}/image_sim.fits" ]]; then
+                        if [[ ! -f "Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}/image_sim.fits" ]] && \
+                            [[ ! -f "Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}/too_faint" ]]; then
                             echo "Error! Failed to run \"almacosmos_simulate_image_based_on_galaxy_modeling\" and create \"Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}/image_sim.fits\" file!"
                             exit 1
                         fi
@@ -255,6 +264,7 @@ for (( i=0; i<${#FitsNames[@]}; i++ )); do
                         # clean
                         if [[ " $@ " != *" test "* ]]; then
                             rm -rf "Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}/$FitsName."*
+                            rm -rf "Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}/$FitsName"
                         fi
                         
                     else
@@ -262,23 +272,6 @@ for (( i=0; i<${#FitsNames[@]}; i++ )); do
                         echo "Found existing \"Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}/image_sim.fits\", skip and continue!"
                         
                     fi
-                    
-                    # do bug fix - apply image_sci_mask to image_sim
-                    #if [[ 1 == 1 ]]; then
-                    #    cd "Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}/$FitsName.cont.I.image/"
-                    #    echo CrabFitsImageArithmetic "image_res.fits" "adds" "image_mod.fits" "image_sim_nonan.fits" ">" "image_sim_nonan.log"
-                    #         CrabFitsImageArithmetic "image_res.fits" "adds" "image_mod.fits" "image_sim_nonan.fits"  >  "image_sim_nonan.log"
-                    #    echo CrabFitsImageArithmetic "image_sci.fits" "times" "0" "image_sci_maskzero.fits" ">" "image_sci_maskzero.log"
-                    #         CrabFitsImageArithmetic "image_sci.fits" "times" "0" "image_sci_maskzero.fits"  >  "image_sci_maskzero.log"
-                    #    echo CrabFitsImageArithmetic "image_sci_maskzero.fits" "adds" "1" "image_sci_mask.fits" ">" "image_sci_mask.log"
-                    #         CrabFitsImageArithmetic "image_sci_maskzero.fits" "adds" "1" "image_sci_mask.fits"  >  "image_sci_mask.log"
-                    #    echo CrabFitsImageArithmetic "image_sim_nonan.fits" "times" "image_sci_mask.fits" "image_sim.fits" ">" "image_sim.log"
-                    #         CrabFitsImageArithmetic "image_sim_nonan.fits" "times" "image_sci_mask.fits" "image_sim.fits"  >  "image_sim.log"
-                    #    #rm image_sci_maskzero.fits image_sim_nonan.fits
-                    #    #rm ../*.log
-                    #    cp image_sim.fits ../
-                    #    cd "../../../../"
-                    #fi
                     
                     
                     # Concat simulated datatable
@@ -298,8 +291,11 @@ for (( i=0; i<${#FitsNames[@]}; i++ )); do
                                     >> "Simulated/$FitsName/datatable_Simulated.txt"
                         
                         echo "Reading \"Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}/galaxy_model_id_ra_dec_flux.txt\""
-                    else
+                        
+                    elif [[ ! -f "Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}/too_faint" ]]; then
+                        
                         echo "Warning! \"Simulated/$FitsName/w_${i_w}_z_${i_z}_lgMstar_${i_lgMstar}_${i_Type_SED}/galaxy_model_id_ra_dec_flux.txt\" was not found! ******"
+                        
                     fi
                     
                 done
