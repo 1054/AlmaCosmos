f5249d2226a478ef44951ba9943a88779f34b3b1
1054 <liudz1054@gmail.com>
2019-06-26 15:29:59 +0200

    Updates

--- Softwares/alma_archive_make_data_dirs_with_meta_table.py
+++ Softwares/alma_archive_make_data_dirs_with_meta_table.py
@@ -420,7 +420,7 @@ if output_table_file != '':
     if output_table_file.find(os.sep) >= 0:
         if not os.path.isdir(os.path.dirname(output_table_file)):
             os.makedirs(os.path.dirname(output_table_file))
-    output_table.write(output_table_file, format='ascii.fixed_width', delimeter='  ', bookend=True)
+    output_table.write(output_table_file, format='ascii.fixed_width', delimiter='  ', bookend=True)
     with open(output_table_file, 'r+') as fp:
         fp.seek(0)
         fp.write('#')
