2ffe1441b184f3ed0457ed726c2989188502b7af
1054 <>
2020-06-03 02:05:00 +0200

    Updates

--- Software/alma_project_level_4_copy_QA2_data_products.bash
+++ Software/alma_project_level_4_copy_QA2_data_products.bash
@@ -16,6 +16,8 @@ for member_dir in "${list_of_member_dirs[@]}"; do
     product_dir=$(ls -1d Level_1_Raw/*/science_goal.*/group.*/member.${mem_ous_id}/product | head -n 1)
 
     script_dir=$(ls -1d Level_1_Raw/*/science_goal.*/group.*/member.${mem_ous_id}/script | head -n 1)
+    
+    readme_file=$(ls -1d Level_1_Raw/*/science_goal.*/group.*/member.${mem_ous_id}/README | head -n 1)
 
     output_dir="Level_4_Data_QA2/member.${mem_ous_id}"
 
@@ -32,6 +34,11 @@ for member_dir in "${list_of_member_dirs[@]}"; do
     if [[ $(find "$script_dir" -maxdepth 1 -mindepth 1 -type f -name "scriptForImaging*.py" | wc -l) -gt 0 ]]; then
         echo "cp $script_dir/scriptForImaging*.py $output_dir/"
         cp $script_dir/scriptForImaging*.py $output_dir/
+    fi
+
+    if [[ -f "$readme_file" ]]; then
+        echo "cp $readme_file $output_dir/"
+        cp $readme_file $output_dir/
     fi
 
 done
--- Software/alma_project_level_5_cleanup.bash
+++ Software/alma_project_level_5_cleanup.bash
@@ -6,6 +6,6 @@ list_of_raw_dirs=($(find "Level_1_Raw" -maxdepth 1 -mindepth 1 -type d -name "*.
 
 echo "Please run this command by yourself:"
 echo "rm -rf ${list_of_raw_dirs[@]}"
-ecoo "rm -rf Level_3_Split/DataSet_*/split_*"
-ecoo "rm -rf Level_4_Data_Images/*/DataSet_*/processing"
+echo "rm -rf Level_3_Split/DataSet_*/split_*"
+echo "rm -rf Level_4_Data_Images/*/DataSet_*/processing"
 
